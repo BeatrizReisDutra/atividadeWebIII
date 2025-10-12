74491bd37e3e06ddf06e2588690e2ed1b03daa35
Beatriz Reis <beatrizreisdutra@gmail.com>
2025-10-12 12:38:54 -0400

    Adicionando rota /usuario e template com passagem de dados

--- main.py
+++ main.py
@@ -4,7 +4,12 @@ app = Flask(__name__)
 
 @app.route('/')
 def homepage():
-    return render_template('index.html')
+    return render_template('home.html')
+
+@app.route("/usuario")
+def dados_usuario():
+    dados_usu = {"nome": "Beatriz", "profissao": "Estudante", "disciplina":"Desenvolvimento Web III"}
+    return render_template("usuario.html", dados = dados_usu)
 
 if __name__ == '__main__':
     app.run(port=8000, debug=True)
