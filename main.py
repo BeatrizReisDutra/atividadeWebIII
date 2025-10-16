ebbab5206942d84f3d2ef83e2f2d93932595ec40
Beatriz Reis <beatrizreisdutra@gmail.com>
2025-10-15 20:26:26 -0400

    tela login flash

--- main.py
+++ main.py
@@ -1,10 +1,46 @@
-from flask import Flask, render_template
-
+from flask import Flask, render_template, request, redirect, url_for, flash
+import re
 app = Flask(__name__)
+app.secret_key = 'sua_chave_secreta_aqui'
+
+REGEX_CARACTER_ESPECIAL = r'[!@#$%^&*()_+=\-{}\[\]:;"\'<>,.?/\\|~`]'
+
+@app.route('/home')
+def homepage():
+    return render_template('home.html')
+
+@app.route('/usuario')
+def dados_usuario():
+    dados_usu = {"nome": "Beatriz", "profissao": "Estudante", "disciplina":"Desenvolvimento Web III"}
+    return render_template("usuario.html", dados = dados_usu)
 
 @app.route('/')
-def homepage():
-    return render_template('index.html')
+def login_get():
+    return render_template('login.html')
+
+@app.route('/login', methods=['POST'])
+def login_post():
+
+    senha = request.form.get('senha')
+
+    if not senha:
+        flash('Senha é obrigatória.', 'error')
+        return redirect(url_for('login_get'))
+    
+    if validar_senha(senha):
+        flash('Login realizado com sucesso!', 'sucesso')
+        return redirect(url_for('homepage'))
+    else:
+        flash('A senha deve conter pelo menos 6 caracteres, incluindo letras maiúsculas, números e caracteres especiais.', 'error')
+        return redirect(url_for('login_get'))
+    
+def validar_senha(senha):
+    if (len(senha) < 6 or
+        not re.search(r'[A-Z]', senha) or
+        not re.search(r'[0-9]', senha) or
+        not re.search(REGEX_CARACTER_ESPECIAL, senha)):
+        return False
+    return True
 
 if __name__ == '__main__':
     app.run(port=8000, debug=True)
